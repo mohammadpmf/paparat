d181cb6fbe42a204e6fd4f58126431698dd77d14
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-11-28 16:20:07 +0330

    give assert a message

--- miscellaneous_codes/raise_yield_assert/assert.py
+++ miscellaneous_codes/raise_yield_assert/assert.py
@@ -1,5 +1,5 @@
 n=int(input("Enter n: "))
-# assert n!=0
-# assert n>10
-# assert n<100
+assert n!=0, "n is 0"
+assert n>10, "n is less than or equal to 10"
+assert n<100, "n is more than or equal to 100"
 print(100/n)
