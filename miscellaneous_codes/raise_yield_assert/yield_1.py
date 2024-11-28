9af6b212b8fdd5580de5fdd6d003a51ecfd13bbc
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-11-28 19:12:56 +0330

    add more lines to yield

--- miscellaneous_codes/raise_yield_assert/yield_1.py
+++ miscellaneous_codes/raise_yield_assert/yield_1.py
@@ -1,11 +1,17 @@
 def simple_generator():
     yield 1
+    print(1)
+    print(2)
+    print(3)
+    print(5555)
     yield 2
+    print(6666)
     yield 3
     yield "reza"
     yield 2.3
     yield True
     yield 4
+    print(7777)
     yield 5
     return
     yield 7
