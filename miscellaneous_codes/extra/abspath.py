ff68e520a706a16ed2ce8a013fe50b23250ae956
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-12-02 20:21:43 +0330

    add abspath

---
+++ miscellaneous_codes/extra/abspath.py
@@ -0,0 +1,6 @@
+from os.path import abspath
+
+print(abspath('test.txt'))
+print('test.txt')
+print(abspath(r'C:\Users\Mohammad\Desktop\My own students\3 Radmehr Bozorgi\38 22 Tir 1403\test.txt'))
+print(r'C:\Users\Mohammad\Desktop\My own students\3 Radmehr Bozorgi\38 22 Tir 1403\test.txt')
