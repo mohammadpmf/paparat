01b98f2e8590e2c2f52fbad48a3c5ac874e1e639
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-12-02 20:39:32 +0330

    add os find

---
+++ miscellaneous_codes/extra/os_find.py
@@ -0,0 +1,7 @@
+import os
+
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+print(find("radmehr", r"C:\Users"))
