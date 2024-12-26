5dd97643d2cd8dee0e53d7bb3280e0e4e8931bd5
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-12-26 17:17:36 +0330

    Add game center with only UI

---
+++ main/8_database/config.py
@@ -0,0 +1,46 @@
+BG = '#333333'
+FG = 'orange'
+
+
+CNF_WINDOWS = {
+    'bg': BG,
+}
+
+
+CNF_BTNS = {
+    'bg': BG,
+    'fg': FG,
+    'font': ('Calibri', 20),
+    'width': 16,
+    'height': 3,
+}
+CNF_BTNS_GRID={
+    'padx': 6,
+    'pady': 4
+}
+CNF_BTNS_SIGN_UP_WINDOW = {
+    'bg': BG,
+    'fg': FG,
+    'font': ('Calibri', 20),
+    'width': 12,
+    'height': 2,
+}
+CNF_BTNS_SIGN_UP_WINDOW_GRID = {
+    'padx': 6,
+    'pady': 10
+}
+
+
+
+CNF_LBLS = {
+    'bg': BG,
+    'fg': FG,
+    'font': ('Calibri', 20),
+}
+
+
+CNF_ENTRY = {
+    'bg': BG,
+    'fg': FG,
+    'font': ('Calibri', 20),
+}
